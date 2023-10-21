458854f8f42408e2d0be647caf7baf087808ca6d
jubayer3112 <you@example.com>
2023-10-21 09:54:07 +0600

    Login and registration form added

--- src/Pages/Register/Register.jsx
+++ src/Pages/Register/Register.jsx
@@ -1,7 +1,178 @@
 const Register = () => {
   return (
-    <div>
-      <h1>Register here</h1>
+    <div className="min-w-screen min-h-screen bg-gray-900 flex items-center justify-center px-5 py-5">
+      <form
+        className="bg-gray-100 text-gray-500 rounded-3xl shadow-xl w-full overflow-hidden"
+        style={{ maxWidth: "1000px" }}
+      >
+        <div className="md:flex w-full">
+          <div className="hidden md:block w-1/2 bg-indigo-500 py-10 px-10">
+            <img
+              className="mx-auto"
+              src="https://www.freeiconspng.com/uploads/register-button-png-0.png"
+              alt=""
+            />
+          </div>
+          <div className="w-full md:w-1/2 py-10 px-5 md:px-10">
+            <div className="text-center mb-10">
+              <h1 className="font-bold text-3xl text-gray-900">REGISTER</h1>
+              <p>Enter your information to register</p>
+            </div>
+            <div>
+              <div className="flex -mx-3">
+                <div className="w-1/2 px-3 mb-5">
+                  <label
+                    htmlFor="firstName"
+                    className="text-xs font-semibold px-1"
+                  >
+                    Name
+                  </label>
+                  <div className="flex">
+                    <div className="w-10 z-10 pl-1 text-center pointer-events-none flex items-center justify-center">
+                      <i className="mdi mdi-account-outline text-gray-400 text-lg"></i>
+                    </div>
+                    <input
+                      type="text"
+                      id="name"
+                      name="firstName"
+                      required
+                      className="w-full -ml-10 pl-3 pr-3 py-2 rounded-lg border-2 border-gray-200 outline-none focus:border-indigo-500"
+                      placeholder="John"
+                    />
+                  </div>
+                </div>
+                <div className="w-1/2 px-3 mb-5">
+                  <label
+                    htmlFor="lastName"
+                    className="text-xs font-semibold px-1"
+                  >
+                    Email
+                  </label>
+                  <div className="flex">
+                    <div className="w-10 z-10 pl-1 text-center pointer-events-none flex items-center justify-center">
+                      <i className="mdi mdi-account-outline text-gray-400 text-lg"></i>
+                    </div>
+                    <input
+                      type="email"
+                      id="lastName"
+                      name="email"
+                      required
+                      className="w-full -ml-10 pl-3 pr-3 py-2 rounded-lg border-2 border-gray-200 outline-none focus-border-indigo-500"
+                      placeholder="johnsmith@example.com"
+                    />
+                  </div>
+                </div>
+              </div>
+              <div className="flex -mx-3">
+                <div className="w-full px-3 mb-5">
+                  <label htmlFor="email" className="text-xs font-semibold px-1">
+                    User Photo (LInk)
+                  </label>
+                  <div className="flex">
+                    <div className="w-10 z-10 pl-1 text-center pointer-events-none flex items-center justify-center">
+                      <i className="mdi mdi-email-outline text-gray-400 text-lg"></i>
+                    </div>
+                    <input
+                      type="url"
+                      id="email"
+                      name="userphoto" // Added name attribute
+                      required // Added required attribute
+                      className="w-full -ml-10 pl-3 pr-3 py-2 rounded-lg border-2 border-gray-200 outline-none focus-border-indigo-500"
+                      placeholder="Your Photo"
+                    />
+                  </div>
+                </div>
+              </div>
+              <div className="flex -mx-3">
+                <div className="w-full px-3 mb-12">
+                  <label
+                    htmlFor="password"
+                    className="text-xs font-semibold px-1"
+                  >
+                    Password
+                  </label>
+                  <div className="flex">
+                    <div className="w-10 z-10 pl-1 text-center pointer-events-none flex items-center justify-center">
+                      <i className="mdi mdi-lock-outline text-gray-400 text-lg"></i>
+                    </div>
+                    <input
+                      type="password"
+                      id="password"
+                      name="password" // Added name attribute
+                      required // Added required attribute
+                      className="w-full -ml-10 pl-3 pr-3 py-2 rounded-lg border-2 border-gray-200 outline-none focus-border-indigo-500"
+                      placeholder="************"
+                    />
+                  </div>
+                </div>
+              </div>
+              <div className="flex -mx-3">
+                <div className="w-full px-3 mb-5">
+                  <button
+                    type="submit"
+                    className="block w-full max-w-xs mx-auto bg-indigo-500 hover:bg-indigo-700 focus-bg-indigo-700 text-white rounded-lg px-3 py-3 font-semibold"
+                  >
+                    REGISTER NOW
+                  </button>
+                </div>
+              </div>
+              <hr className="my-6 border-gray-300 w-full" />
+              <button
+                type="button"
+                className="w-full block bg-white hover:bg-gray-100 focus:bg-gray-100 text-gray-900 font-semibold rounded-lg px-4 py-3 border border-gray-300"
+              >
+                <div className="flex items-center justify-center">
+                  <svg
+                    xmlns="http://www.w3.org/2000/svg"
+                    className="w-6 h-6"
+                    viewBox="0 0 48 48"
+                  >
+                    <defs>
+                      <path
+                        id="a"
+                        d="M44.5 20H24v8.5h11.8C34.7 33.9 30.1 37 24 37c-7.2 0-13-5.8-13-13s5.8-13 13-13c3.1 0 5.9 1.1 8.1 2.9l6.4-6.4C34.6 4.1 29.6 2 24 2 11.8 2 2 11.8 2 24s9.8 22 22 22c11 0 21-8 21-22 0-1.3-.2-2.7-.5-4z"
+                      />
+                    </defs>
+                    <clipPath id="b">
+                      <use xlinkHref="#a" overflow="visible" />
+                    </clipPath>
+                    <path
+                      clipPath="url(#b)"
+                      fill="#FBBC05"
+                      d="M0 37V11l17 13z"
+                    />
+                    <path
+                      clipPath="url(#b)"
+                      fill="#EA4335"
+                      d="M0 11l17 13 7-6.1L48 14V0H0z"
+                    />
+                    <path
+                      clipPath="url(#b)"
+                      fill="#34A853"
+                      d="M0 37l30-23 7.9 1L48 0v48H0z"
+                    />
+                    <path
+                      clipPath="url(#b)"
+                      fill="#4285F4"
+                      d="M48 48L17 24l-4-3 35-10z"
+                    />
+                  </svg>
+                  <span className="ml-4">Log in with Google</span>
+                </div>
+              </button>
+              <p className="mt-8">
+                Already have an account?{" "}
+                <a
+                  href="#"
+                  className="text-blue-500 hover:text-blue-700 font-semibold"
+                >
+                  Login
+                </a>
+              </p>
+            </div>
+          </div>
+        </div>
+      </form>
     </div>
   );
 };
